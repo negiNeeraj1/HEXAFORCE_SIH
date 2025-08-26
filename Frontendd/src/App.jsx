d55a180ccc2b56b8e9a87486f28bed4b81f186db
joshiRakshit12 <rakshitjoshi5678@gmail.com>
2025-08-26 22:16:11 +0530

    add animation

--- Frontendd/src/App.jsx
+++ Frontendd/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Design from '../Design';
+import Icon from './Icon';
 
 function App() {
 	return (
 		<div>
 			<Design />
+			<div>
+				<Icon />
+			</div>
+			{/* <Icon /> */}
 
 			<div>
 				<h1 className="bg-amber-700 text-white p-2 rounded">hello</h1>
